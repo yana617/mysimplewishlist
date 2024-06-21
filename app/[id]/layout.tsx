7fc19a902129512aae1751a701470d5890f5277b
Yana Sidarava <yanasidarava@MacBook-Pro-Yana-2.local>
2024-06-21 22:57:00 +0300

    fix: styles

--- app/[id]/layout.tsx
+++ app/[id]/layout.tsx
@@ -2,7 +2,7 @@ import { Header } from "@/components/Header";
 
 export default function Layout({ children, params }: { children: React.ReactNode; params: { id: string } }) {
     return (
-        <div className="w-full h-screen bg-base-200">
+        <div className="w-full h-screen">
             <Header listId={params.id} />
             {children}
         </div>
--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,6 +1,6 @@
 export const Loading = () => {
     return (
-        <div className='animate-in flex max-w-4xl flex-1 flex-col items-start justify-center gap-20 px-3 opacity-0'>
+        <div className='animate-in flex w-full h-full items-center justify-center'>
             <span className='loading loading-spinner loading-lg'></span>
         </div>
     );
