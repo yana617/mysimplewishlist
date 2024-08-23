200fd45728a0c5ca647d1f01df1854ad1d738e03
Yana <ysidarava@gmail.com>
2024-08-24 02:00:46 +0300

    fix: linter

--- src/app/home/[id]/page.tsx
+++ src/app/home/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { WishlistPage } from '@/src/fsd-pages/wishlist';
+import { WishlistPage } from '@/src/views/wishlist';
 
 export default async function Page({ params }: { params: Record<string, string> }) {
     return <WishlistPage params={params} />;
